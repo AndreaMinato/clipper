57d5ff2a4dd3b8b19a7a58ab26c5238edddae3e3
Andrea Minato <aminato@tempestive.com>
2020-08-05 14:56:32 +0200

    Vue3 configurations with linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,34 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/vue3-essential',
-    '@vue/standard'
+    '@vue/standard',
+    'eslint:recommended',
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    'comma-dangle': ['error', {
+      arrays: 'always',
+      objects: 'always',
+      imports: 'never',
+      exports: 'never',
+      functions: 'never',
+    },],
+    'semi': ['error', 'always']
+  },
 }
