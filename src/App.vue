0bde0d95f9c3e1f0d9e85309a31e294118ede3d9
Andrea Minato <15440218+AndreaMinato@users.noreply.github.com>
2021-12-16 11:48:24 +0100

    Cose brutte da togliere

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div  class="bg-gray-50 min-h-screen max-h-screen w-screen flex flex-col items-stretch min-w-lg">
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 sm:pt-1 md:pt-4 lg:pt-12 flex flex-col items-stretch flex-1 overflow-y-auto w-full">
-      <router-view />
+    Ciao
+    
+    <router-view />
     </div>
   </div>
 </template>
